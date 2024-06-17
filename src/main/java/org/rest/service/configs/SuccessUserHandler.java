5bd2b6adeb64eb5e46e7173f91e867ccbb4f67e1
UseR <147231882+BogdanPryadko4853@users.noreply.github.com>
2024-06-17 14:47:50 +0300

    add security(WebSecurityConfig)

---
+++ src/main/java/org/rest/service/configs/SuccessUserHandler.java
@@ -0,0 +1,26 @@
+package org.rest.service.configs;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Set;
+
+@Component
+public class SuccessUserHandler implements AuthenticationSuccessHandler {
+    // Spring Security использует объект Authentication, пользователя авторизованной сессии.
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException {
+        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+        if (roles.contains("ADMIN")) {
+            httpServletResponse.sendRedirect("/api");
+        } else if (roles.contains("USER")) {
+            httpServletResponse.sendRedirect("/api");
+        }
+
+    }
+}
---
+++ src/main/java/org/rest/service/configs/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package org.rest.service.configs;
+
+import org.rest.service.service.CustomUserDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomUserDetailService customUserDetailService;
+    private final SuccessUserHandler successUserHandler;
+    @Autowired
+    public WebSecurityConfig(SuccessUserHandler successUserHandler, CustomUserDetailService customUserDetailService) {
+        this.successUserHandler = successUserHandler;
+        this.customUserDetailService = customUserDetailService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/**").hasAuthority("ADMIN")
+                .antMatchers("/api/**").hasAnyAuthority("ADMIN", "USER")
+                .antMatchers("/registration").not().fullyAuthenticated()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").successHandler(successUserHandler)
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(customUserDetailService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
+}
