0e40a747be6c74d2b0773174f0a4e3ca01e75eec
UseR <147231882+BogdanPryadko4853@users.noreply.github.com>
2024-06-17 13:47:47 +0300

    initialize

---
+++ src/main/java/org/rest/service/EmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package org.rest.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rest/service/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rest.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
