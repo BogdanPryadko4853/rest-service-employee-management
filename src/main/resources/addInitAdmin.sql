4ba6e93cbce60f4645a84f3461782a77db1d01f0
UseR <147231882+BogdanPryadko4853@users.noreply.github.com>
2024-06-17 14:55:45 +0300

    add 'addInitAdmin.sql' sql query

---
+++ src/main/resources/addInitAdmin.sql
@@ -0,0 +1,10 @@
+
+
+INSERT INTO users(id, age, email, password, username)
+VALUES (7, 20, 'ghfdfvb@gmail.com', '$2a$12$.Dgch2xBkGtx9X7aBXoyxOMQZmJE3ENVRK0V6GmdxiDYuO0y5xVYS', 'user');
+
+INSERT INTO user_role(user_id, role_id)
+VALUES (7, 1);
+
+
+-- нужно вводить в качестве username - user , a password - password
